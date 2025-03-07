1dfc088493865025322a77664ffef20d44b9024b
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-03-07 16:27:56 +0000

    checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/disposer/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/disposer/SampleFunctionalTest.java
@@ -1,20 +1,13 @@
 package uk.gov.hmcts.reform.disposer;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.disposer.service.ServiceTokenGenerator;
-
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("functional")
