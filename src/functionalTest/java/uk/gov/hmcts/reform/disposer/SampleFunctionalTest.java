2c3c2a5a0ceb356a9f375373a464e6bea23131e8
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-03-07 15:33:29 +0000

    LAU-1105 add functional test to test s2s token

--- src/functionalTest/java/uk/gov/hmcts/reform/disposer/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/disposer/SampleFunctionalTest.java
@@ -3,37 +3,29 @@ package uk.gov.hmcts.reform.disposer;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.disposer.service.ServiceTokenGenerator;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest
+@ActiveProfiles("functional")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 class SampleFunctionalTest {
-    protected static final String CONTENT_TYPE_VALUE = "application/json";
 
-    @Value("${TEST_URL:http://localhost:8080}")
-    private String testUrl;
-
-    @BeforeEach
-    public void setUp() {
-        RestAssured.baseURI = testUrl;
-        RestAssured.useRelaxedHTTPSValidation();
-    }
+    private final ServiceTokenGenerator serviceTokenGenerator;
 
     @Test
-    void functionalTest() {
-        Response response = given()
-            .contentType(ContentType.JSON)
-            .when()
-            .get()
-            .then()
-            .extract().response();
-
-        Assertions.assertEquals(200, response.statusCode());
-        Assertions.assertTrue(response.asString().startsWith("Welcome"));
+    void itShouldGenerateServiceToken() {
+        serviceTokenGenerator.generateToken();
+        assertThat(serviceTokenGenerator.getServiceToken()).startsWith("Bearer eyJhbGciOiJIUzUxMiJ9");
     }
 }
